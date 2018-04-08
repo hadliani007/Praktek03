810204b72ddfb5b0d73236b9d3aedcb54d8f0842
hadliani007 <hadliani007@gmail.com>
2018-04-08 14:24:12 +0800

    instance, buat tribut dan panggil method

--- src/praktek03/TopiAksi.java
+++ src/praktek03/TopiAksi.java
@@ -1,7 +1,12 @@
-package praktek03;
+package Praktek03;
 public class TopiAksi {
     public static void main(String[] args) {
+        Topi t = new Topi();
+        t.merk="Okley";
+        t.warna="Hijau";
+        t.harga=15000;
         
+        t.cetakInfo();
     }
     
 }
