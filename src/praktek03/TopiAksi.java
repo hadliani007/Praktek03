0d04a7c4caefcc835cf0dbfbc5235d05a54e55df
hadliani007 <hadliani007@gmail.com>
2018-04-08 14:44:07 +0800

    menggunakan method sett

--- src/praktek03/TopiAksi.java
+++ src/praktek03/TopiAksi.java
@@ -2,9 +2,9 @@ package Praktek03;
 public class TopiAksi {
     public static void main(String[] args) {
         Topi t = new Topi();
-        t.merk="Okley";
-        t.warna="Hijau";
-        t.harga=15000;
+        t.setMerk="Okley";
+        t.setWarna="Hijau";
+        t.setHarga=15000;
         
         t.cetakInfo();
     }
